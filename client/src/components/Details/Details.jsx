0256391e8c1787515dcdd20a420fc08a871addcb
POrtalda <paoloortalda@gmail.com>
2025-08-24 23:30:44 +0200

    aggiunta dei semafori nel dettaglio

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -4,11 +4,11 @@ import './Details.css';
 export default function Details({ children, vehicles }) {
 
     // devo recuperare l'id del veicolo dalla url
-    const {id} = useParams();
+    const { id } = useParams();
 
     // devo trovare i dati del veicolo con quell' id
     const vehicle = vehicles.find(v => v.id.toString() === id.toString());
-    
+
     // Se non ho ancora i dati o il veicolo non esiste
     if (!vehicle) {
         return (
@@ -27,6 +27,19 @@ export default function Details({ children, vehicles }) {
             <div className='card-details'>
                 <h1>{`${vehicle.brand} ${vehicle.model}`}</h1>
                 <img src={vehicle.img_url} alt={`${vehicle.brand} ${vehicle.model}`} />
+                <h3>
+                    {`Scadenza revisione: ${vehicle.scadenza_revisione} ${vehicle.expired_revision ? '🔴' : (vehicle.expiring_revision ? '🟠' : '🟢')
+                        }`}
+                </h3>
+
+                <h3>
+                    {`Scadenza bollo: ${vehicle.scadenza_bollo} ${vehicle.expired_car_tax ? '🔴' : (vehicle.expiring_car_tax ? '🟠' : '🟢')
+                        }`}
+                </h3>
+                <h3>
+                    {`Scadenza assicurazione: ${vehicle.scadenza_assicurazione} ${vehicle.expired_insurance ? '🔴' : (vehicle.expiring_insurance ? '🟠' : '🟢')
+                        }`}
+                </h3>
             </div>
         </>
     );
